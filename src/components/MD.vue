7bd36fe5e11dee0d34ab0f75a63fc389cedb6d5f
bobo <zannb@outlook.com>
2017-09-17 13:19:01 +0800

    switch routes to mode 'history';

--- src/components/MD.vue
+++ src/components/MD.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="md">
-    <div id="raw-md">
-      <textarea v-model="rawMD"></textarea>
+  <div id="editor-holder">
+    <div id="editor">
+      <textarea v-model="rawMD" placeholder="Write the markdown here..."></textarea>
+      <div v-html="renderedMD"></div>
     </div>
-    <div id="gen-md" v-html="renderedMD"></div>
   </div>
 </template>
 
@@ -11,13 +11,13 @@
 import marked from 'marked'
 export default {
   name: 'md',
-  data () {
+  data() {
     return {
-      rawMD: 'Write the markdown here...'
+      rawMD: ''
     }
   },
   computed: {
-    renderedMD () {
+    renderedMD() {
       return marked(this.rawMD, { sanitize: true })
     }
   }
@@ -26,22 +26,46 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1,
-h2 {
-  font-weight: normal;
+html,
+body,
+#editor{
+  margin: 0;
+  height: 100%;
+  font-family: 'Helvetica Neue', Arial, sans-serif;
+  color: #333;
+  text-align: left;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+#editor div p {
+  font-size: 14px;
 }
 
-li {
+#editor-holder {
+  height: 400px;
+}
+
+textarea,
+#editor div {
   display: inline-block;
-  margin: 0 10px;
+  width: 49%;
+  height: 100%;
+  vertical-align: top;
+  box-sizing: border-box;
+  padding: 0 20px;
 }
 
-a {
-  color: #42b983;
+textarea{
+  border: none;
+  border-right: 1px solid #ccc;
+  resize: none;
+  outline: none;
+  background-color: #f6f6f6;
+  font-size: 14px;
+  font-family: 'Monaco', courier, monospace;
+  padding: 20px;
+}
+
+code {
+  color: #f66;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Hello from '@/components/Hello'
+import Hello from '@/components/Hello'
 import MD from '@/components/MD'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: MD
-    },
     {
       path: '/md',
       name: 'MD',
       component: MD
+    },
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
     }
+    // {
+    //   path: '/md',
+    //   name: 'MD',
+    //   component: MD
+    // }
   ]
 })
