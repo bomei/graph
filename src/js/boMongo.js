9fbf66afa5b4e42f922675551e4007f04026405a
bobo <zannb@outlook.com>
2017-10-01 23:12:16 +0800

    pack for MongoClient

--- src/js/boMongo.js
+++ src/js/boMongo.js
@@ -6,13 +6,84 @@ class BoMongo {
     this.url = `mongodb://localhost:27017/${db}`
   }
 
-  insert(collection, dataBundle) {
+  insert(collection, dataBundle, callback) {
     MongoClient.connect(this.url, (err, db) => {
       assert.equal(null, err)
 
       db.collection(collection).insertMany(dataBundle, (err, r) => {
-        assert.equal(null, err)
-        assert.equal(dataBundle.length, r.insertedCount)
+        callback(err, r)
+      })
+
+      db.close()
+    })
+  }
+
+  updateOne(collection, get, set, callback) {
+    MongoClient.connect(this.url, (err, db) => {
+      assert.equal(null, err)
+
+      db.collection(collection).updateOne(get, set, (err, r) => {
+        callback(err, r)
+      })
+
+      db.close()
+    })
+  }
+
+  updateMany(collection, get, set, callback) {
+    MongoClient.connect(this.url, (err, db) => {
+      assert.equal(null, err)
+
+      db.collection(collection).updateMany(get, set, (err, r) => {
+        callback(err, r)
+      })
+
+      db.close()
+    })
+  }
+
+  deleteOne(collection, get, callback) {
+    MongoClient.connect(this.url, (err, db) => {
+      assert.equal(null, err)
+
+      db.collection(collection).deleteOne(get, (err, r) => {
+        callback(err, r)
+      })
+
+      db.close()
+    })
+  }
+
+  deleteMany(collection, get, callback) {
+    MongoClient.connect(this.url, (err, db) => {
+      assert.equal(null, err)
+
+      db.collection(collection).deleteMany(get, (err, r) => {
+        callback(err, r)
+      })
+
+      db.close()
+    })
+  }
+
+  findOneAndDelete(collection, get, option = {}, callback) {
+    MongoClient.connect(this.url, (err, db) => {
+      assert.equal(null, err)
+
+      db.collection(collection).findOneAndDelete(get, option, (err, r) => {
+        callback(err, r)
+      })
+
+      db.close()
+    })
+  }
+
+  findOneAndUpdate(collection, get, set, option = {}, callback) {
+    MongoClient.connect(this.url, (err, db) => {
+      assert.equal(null, err)
+
+      db.collection(collection).findOneAndUpdate(get, set, option, (err, r) => {
+        callback(err, r)
       })
 
       db.close()
--- src/js/testBoMongo.js
+++ src/js/testBoMongo.js
@@ -1,4 +1,29 @@
 const BoMongo = require('./boMongo')
+const assert = require('assert')
 
 let boMongo = new BoMongo('bo')
-boMongo.insert('hello', [{a: 1, b: 2}])
+
+function testInsert() {
+  boMongo.insert('hello', [{
+    a: 1,
+    b: 2
+  }])
+}
+
+function testUpdateOne() {
+  boMongo.updateOne('hello', {a: 1}, {$set: {c: 2}}, (err, r) => {
+    assert.equal(null, err)
+    console.log(`Matched count: ${r.matchedCount}`)
+    console.log(`Modified count: ${r.modifiedCount}`)
+  })
+}
+
+function testUpdateMany() {
+  boMongo.updateMany('hello', {a: 1}, {$set: {d: 2}}, (err, r) => {
+    assert.equal(null, err)
+    console.log(`Matched count: ${r.matchedCount}`)
+    console.log(`Modified count: ${r.modifiedCount}`)
+  })
+}
+
+testUpdateMany()
